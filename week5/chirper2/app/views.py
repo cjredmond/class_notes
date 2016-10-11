3f10ae9651c361a4ebda5de645497ffd70e6e817
cjredmond <connor.redmond@gmail.com>
2016-10-11 13:35:07 -0400

    chirper app done

---
+++ week5/chirper2/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from app.models import Chirp
+
+def index_view(request):
+    print(request.POST)
+    if request.POST:
+        chirp_body = request.POST["chirp_body"]
+        if chirp_body != "":
+            Chirp.objects.create(body=chirp_body)
+    context = {
+    "chirps" : Chirp.objects.all().order_by("-created")
+    }
+    return render(request, "index.html", context)
+
+def about_view(request):
+    return render(request, "about.html")
