55dae5d0b1f4a24a73f0a39b0b405277ea6a871c
cjredmond <connor.redmond@gmail.com>
2016-10-13 13:16:54 -0400

    end day 4

--- week5/chirper2/app/views.py
+++ week5/chirper2/app/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from app.models import Chirp
 from app.forms import ChirpForm
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
+
 def index_view(request):
     if request.POST:
         instance = ChirpForm(request.POST)
@@ -10,7 +12,7 @@ def index_view(request):
             instance.save()
 
     context = {
-    "chirps" : Chirp.objects.all().order_by("-created"),
+    "chirps" : Chirp.objects.all(),
     "form" : ChirpForm()
     }
     return render(request, "index.html", context)
@@ -26,5 +28,17 @@ def about_view(request):
 
 class ChirpView(ListView):
     template_name = "chirps.html"
-
     model = Chirp
+
+class ChirpDetailView(DetailView):
+    model = Chirp
+
+class ChirpCreateView(CreateView):
+    model = Chirp
+    success_url = "/chirps"
+    fields = ('body',)
+
+class ChirpUpdateView(UpdateView):
+    model = Chirp
+    success_url = "/chirps"
+    fields = ('body',)
