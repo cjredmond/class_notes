32c8ec28329095a44ff84e098c57026017cd1dfb
cjredmond <connor.redmond@gmail.com>
2016-10-13 09:51:20 -0400

    added first class base view

--- week5/chirper2/app/views.py
+++ week5/chirper2/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app.models import Chirp
 from app.forms import ChirpForm
+from django.views import View
 
 def index_view(request):
     if request.POST:
@@ -22,3 +23,10 @@ def about_view(request):
     voice = request.POST.get("voice", "")
     print(message,voice)
     return render(request, "about.html")
+
+class ChirpView(View):
+    def get(self, request):
+        return render(request, "chirps.html")
+
+    def post(self, request):
+        return render(request, "chirps.html")
