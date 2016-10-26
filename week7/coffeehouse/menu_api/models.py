b4925c7f1cf65941cf51ca6f0ab1d6249f9732a6
cjredmond <connor.redmond@gmail.com>
2016-10-26 11:56:34 -0400

    new api stuff

--- week7/coffeehouse/menu_api/models.py
+++ week7/coffeehouse/menu_api/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    calories = models.IntegerField()
+
 class Special(models.Model):
     name = models.CharField(max_length=48)
     price = models.FloatField()
     description = models.CharField(max_length=100)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    @property
+    def calorie_count(self):
+        return sum(self.ingredients.all().values_list("calories", flat=True))
+
+#('menu_api.Ingredient')
--- week7/coffeehouse/menu_api/serializers.py
+++ week7/coffeehouse/menu_api/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
-from menu_api.models import Special
+from menu_api.models import Special, Ingredient
 
 
 
 class SpecialSerializer(serializers.ModelSerializer):
+    ingredients = serializers.HyperlinkedRelatedField(
+        view_name = "ingredients_detail_api_view",
+        many = True,
+        read_only = True
+
+    )
+    calorie_count = serializers.IntegerField()
+    my_fave = serializers.SerializerMethodField()
+
+    def get_my_fave(self, obj):
+        return "Cookies"
+
     class Meta:
         model = Special
         fields = '__all__'
-        
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
