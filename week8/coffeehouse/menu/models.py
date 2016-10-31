11414f11e38d45424e1fbe2646f4ed2b5f759da6
cjredmond <connor.redmond@gmail.com>
2016-10-31 11:53:32 -0400

    started week 8

--- week8/coffeehouse/menu/models.py
+++ week8/coffeehouse/menu/models.py
@@ -5,6 +5,13 @@ class Special(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
+    picture = models.FileField()
 
     def __str__(self):
         return str(self.title)
+
+    @property
+    def image_url(self):
+        if self.picture:
+            return self.picture.url
+        return "http://images.clipartpanda.com/animated-question-mark-for-powerpoint-1256186461796715642question-mark-icon.svg.hi.png"
